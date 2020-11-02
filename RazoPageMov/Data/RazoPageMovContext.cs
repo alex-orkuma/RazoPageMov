d2644a7004c4e3068a7586a2c3dd531458912d45
Shiaondo Orkuma <shiaondoorkuma@gmail.com>
2020-11-02 11:36:03 +0100

    Add project files.

---
+++ RazoPageMov/Data/RazoPageMovContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazoPageMov.Models;
+
+namespace RazoPageMov.Data
+{
+    public class RazoPageMovContext : DbContext
+    {
+        public RazoPageMovContext (DbContextOptions<RazoPageMovContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazoPageMov.Models.Movie> Movie { get; set; }
+    }
+}
